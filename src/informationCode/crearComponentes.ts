781445e77bc24b14a4a8e60b06810a8c5fe6baba
KevinEstradaAlonso <97643663+KevinEstradaAlonso@users.noreply.github.com>
2022-01-12 20:00:27 -0600

    Delete src directory

--- src/informationCode/crearComponentes.ts
+++
@@ -1,8 +0,0 @@
-//CREACION DE UN COMPONENTE//
-
-//CREAMOS UNA CONSOLO SOBRE LA CARPETA COMPONENTS
-
-//EJECUTAMOS EL SIGUIENTE CODIGO 
-//EN ESTE EJEMPLO SE CREARA EL COMPONENTE pagePresentation
-//ng g m pagePresentation --module app --route pagePresentation
-
